c50c22a7028c93e3f0436244afe8aaa6f5f9e4b4
tiagomelo784-source <tiagomelo784@gmail.com>
2025-11-05 20:53:15 +0000

    Ajustar layout para PT-PT

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,13 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Fast Deliveries',
-  description: 'Entrega em até 7 dias!',
+  title: 'Fast Deliveries - Entregas Rápidas',
+  description: 'Produtos com entrega em até 7 dias úteis!',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-PT">
       <body className={inter.className}>{children}</body>
     </html>
   );
